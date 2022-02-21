2e5679f18fd499ae603572c7abc4a0d8f214b47e
Rehan Shekh <rehanrshekh@gmail.com>
2022-02-21 23:18:38 +0530

    First Push

---
+++ Class Problems/src/com/blz/EmpWage3.java
@@ -0,0 +1,25 @@
+package com.blz;
+
+public class EmpWage3 {
+
+        public static void main(String[] args) {
+        int IS_PRESENT = 1;
+        int IS_PART_TIME = 2;
+        int WAGE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empwage = 0;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PRESENT)
+            empHrs = 8;
+        else if (empcheck == IS_PART_TIME)
+            empHrs = 4;
+        else
+            empHrs = 0;
+        empwage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage:" + empwage);
+
+    }
+
+    }
+
+
