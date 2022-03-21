28b16db24cf3f4cee6b8b1620135f42a5ebb6e6f
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-21 20:10:34 +0530

    Added 2DArray Program

---
+++ com/blz/TwoDArray.java
@@ -0,0 +1,37 @@
+package com.blz;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number of rows(M):");
+        Scanner in1 = new Scanner(System.in);
+        int rows = in1.nextInt();
+        System.out.println("Enter the number of columns(N):");
+        Scanner in2 = new Scanner(System.in);
+        int cols = in2.nextInt();
+        int[][] twoD_arr = new int[rows][cols];
+        Scanner in3 = new Scanner(System.in);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.println("Enter the number to be stored in rows(" + i + ")" + " and column(" + j + ")");
+                twoD_arr[i][j] = in3.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                System.out.print(twoD_arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        PrintWriter writer = new PrintWriter(System.out);
+        writer.write("Above is the Output of the 2D Array");
+        writer.flush();
+        writer.close();
+    }
+}
+
